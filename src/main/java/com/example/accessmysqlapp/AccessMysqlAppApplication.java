9357bb750fcf41e9b47d9c926ee64c45c85cc288
nguyennvm <minhnguyen_ttc@yahoo.com>
2021-08-23 23:34:14 +0700

    first commit

---
+++ src/main/java/com/example/accessmysqlapp/AccessMysqlAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessmysqlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessMysqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessMysqlAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accessmysqlapp/AccessMysqlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessmysqlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessMysqlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
